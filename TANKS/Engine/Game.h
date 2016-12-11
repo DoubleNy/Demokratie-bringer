20a207561d831df5a7c8013f02d82255434d07c1
DoubleNy <1doubleny@gmail.com>
2016-12-11 12:12:11 +0200

    Mici ajustari si posibilitatea de a muta un obiect in fereastra aplicatiei.

---
+++ TANKS/Engine/Game.h
@@ -0,0 +1,28 @@
+#pragma once
+#include "Keyboard.h"
+#include "Mouse.h"
+#include "Graphics.h"
+
+class Game
+{
+public:
+	Game( class MainWindow& wnd );
+	Game( const Game& ) = delete;
+	Game& operator=( const Game& ) = delete;
+	void Go();
+	//
+	void Draw(int posx, int posy);
+private:
+	int posx = 400, posy = 400;
+	void ComposeFrame();
+	void UpdateModel();
+	/********************************/
+	/*  User Functions              */
+	/********************************/
+private:
+	MainWindow& wnd;
+	Graphics gfx;
+	/********************************/
+	/*  User Variables              */
+	/********************************/
+};
