35c0766c40ee1772ad6db5e9fa6fea0776ace4fa
DoubleNy <1doubleny@gmail.com>
2017-01-05 10:39:11 -0200

    explosion,fire,first map

--- TanksSFML/TanksSFML/game.cpp
+++ TanksSFML/TanksSFML/game.cpp
@@ -7,26 +7,41 @@
 #include "keyboard.h"
 #include "objects.hpp"
 #include "attack.hpp"
+#include <string>
+#include <sstream>
 
 void game() {
 
 	// Creare fereastra de joc, dimensiune 924 x 704 FINALLY?
 	sf::RenderWindow window(sf::VideoMode(924, 704), "Tanks ");
+	sf::RenderWindow details(sf::VideoMode(924, 100), "Details");
 	// Creare jucator
 	sf::RectangleShape player(sf::Vector2f(80.0f, 65.0f));
+	sf::RectangleShape heartOne(sf::Vector2f(50, 35));
+	sf::RectangleShape heartTwo(sf::Vector2f(50, 35));
+	sf::RectangleShape exploded(sf::Vector2f(80, 80));
 	// Creare dusman
 	sf::RectangleShape enemy(sf::Vector2f(80.0f, 65.0f));
 	sf::RectangleShape fire(sf::Vector2f(20, 20));
 	//Pozitie de start pentru jucator
 	player.setPosition(-8.0, 0.0);
 	//Pozitie de start pentru dusman
+	details.setPosition(sf::Vector2i(500, 50));
 	enemy.setPosition(860, 600);
+	sf::Texture heart;
+	heart.loadFromFile("heart.png");
 	sf::Texture texture;
 	sf::Texture firetext;
+	sf::Texture expl; 
 	firetext.loadFromFile("fire.png");
+	expl.loadFromFile("exploded.png");
+	exploded.setTexture(&expl);
 	texture.loadFromFile("tank.png"/*, sf::IntRect(0, 0, 80, 65)*/);
 	sf::Texture texture2;
 	texture2.loadFromFile("tank.png"/*, sf::IntRect(0, 0, 80, 65)*/);
+	heartOne.setTexture(&heart);
+	heartTwo.setTexture(&heart);
+
 	int x = 0, a = 79, b = 63;
 	int x0 = 70;
 	//directia dusmanului ,  -1 = nord, 1 = sud
@@ -41,7 +56,76 @@ void game() {
 		return;
 	}
 	//
+	sf::Text playerHealth;
+	sf::Text enemyHealth;
+	sf::Text playerName;
+	sf::Text enemyName;
+	//
+
+
 	sf::Image icon;
+	sf::Text text;
+	sf::Text Win;
+	sf::Text fps;
+	sf::Font font;
+	if (!font.loadFromFile("arial.ttf"))
+	{
+		std::cout << "O eroare cu fontul ... " << std::endl;
+		std::cin.get();
+		return;
+	}
+	Win.setFont(font);
+	Win.setString("YOU WIN !!!");
+	Win.setCharacterSize(70);
+	Win.setFillColor(sf::Color::White);
+	Win.setStyle(sf::Text::Bold);
+
+
+	playerHealth.setFont(font);
+	playerHealth.setString("100");
+	playerHealth.setCharacterSize(20);
+	playerHealth.setFillColor(sf::Color::White);
+	playerHealth.setStyle(sf::Text::Bold);
+
+	enemyHealth.setFont(font);
+	enemyHealth.setString("100");
+	enemyHealth.setCharacterSize(20);
+	enemyHealth.setFillColor(sf::Color::White);
+	enemyHealth.setStyle(sf::Text::Bold);
+
+	playerName.setFont(font);
+	playerName.setString("PLAYER:");
+	playerName.setCharacterSize(20);
+	playerName.setFillColor(sf::Color::White);
+	playerName.setStyle(sf::Text::Bold);
+
+	enemyName.setFont(font);
+	enemyName.setString("ENEMY:");
+	enemyName.setCharacterSize(20);
+	enemyName.setFillColor(sf::Color::White);
+	enemyName.setStyle(sf::Text::Bold);
+
+	text.setFont(font);
+	text.setString("30 ");
+	text.setCharacterSize(20);
+	text.setFillColor(sf::Color::White);
+	text.setStyle(sf::Text::Bold);
+
+	fps.setFont(font);
+	fps.setString("FPS");
+	fps.setCharacterSize(20);
+	fps.setFillColor(sf::Color::White);
+	fps.setStyle(sf::Text::Bold);
+	fps.setPosition(40, 0);
+
+	//
+	playerName.setPosition(400, 0);
+	playerHealth.setPosition(500, 0);
+	heartOne.setPosition(340, 0);
+	enemyName.setPosition(700, 0);
+	enemyHealth.setPosition(800, 0);
+	heartTwo.setPosition(640, 0);
+	int pHealth = 100, eHealth = 100;
 	icon.loadFromFile("icon.png");
 	window.setIcon(icon.getSize().x, icon.getSize().y, icon.getPixelsPtr());
 	//
@@ -60,10 +144,10 @@ void game() {
 		sf::Event event;
 		while (window.pollEvent(event)) {
 			if (event.type == sf::Event::Closed)
-				window.close();
+				window.close(), details.close();
 			else
 				if (KeyEsc())
-					window.close();
+					window.close(), details.close();
 		}	
 			if (KeyUp()) {
 				case0 = 0;
@@ -131,13 +215,9 @@ void game() {
 			pressed = 1;
 		}
 		window.clear();
+		details.clear();
 		//std::cout << player.getPosition().x << std::endl;
-		//Desenam harta
-		window.draw(map);
-		//Desenam jucatorul
-		window.draw(enemy);
-		//Desenam dusmanul
-		window.draw(player);
+		
 		//window.draw(fire);
 		if (pressed && !setted1 && !setted2 && !setted3 && !setted4) {
 			attack(fire, player, enemy, case0);
@@ -152,42 +232,101 @@ void game() {
 			
 		}
 		//Ii afisam pe toti
+		//Desenam harta
+		window.draw(map);
+		//Desenam jucatorul
+		//if (eHealth > 0)
+			window.draw(enemy);
+		//else {
+	//		exploded.setPosition(enemy.getPosition().x, enemy.getPosition().y);
+//			window.draw(exploded);
+//		}
+		//Desenam dusmanul
+		window.draw(player);
 		if (setted1 && !setted2 && !setted3 && !setted4) {
 			fire.move(0, -15);
 			window.draw(fire);
-			if (fire.getPosition().y < 10 || intersect(enemy.getPosition().x, enemy.getPosition().y, 20, fire.getPosition().x, fire.getPosition().y, 20) || obstacleUp(fire.getPosition().x, fire.getPosition().y))
+			if (fire.getPosition().y < 10 || intersect(enemy.getPosition().x, enemy.getPosition().y, 20, fire.getPosition().x, fire.getPosition().y, 20)
+				|| obstacleUp(fire.getPosition().x, fire.getPosition().y)) {
 				setted1 = 0;
+				if (intersect(enemy.getPosition().x, enemy.getPosition().y, 20, fire.getPosition().x, fire.getPosition().y, 20))
+					eHealth -= 10;
+			}
 		}  else
 		if (setted2 && !setted1 && !setted3 && !setted4) {
 				fire.move(0, 15);
 				window.draw(fire);
-				if (fire.getPosition().y > window.getSize().y - 10 || intersect(enemy.getPosition().x, enemy.getPosition().y, 20, fire.getPosition().x, fire.getPosition().y, 20) || obstacleDown(fire.getPosition().x, fire.getPosition().y - 40))
+				if (fire.getPosition().y > window.getSize().y - 10 || intersect(enemy.getPosition().x, enemy.getPosition().y, 20, fire.getPosition().x, fire.getPosition().y, 20)
+					|| obstacleDown(fire.getPosition().x, fire.getPosition().y - 40)) {
 					setted2 = 0;
+					if (intersect(enemy.getPosition().x, enemy.getPosition().y, 20, fire.getPosition().x, fire.getPosition().y, 20))
+						eHealth -= 10;
+				}
 		}  else
 		if (setted3 && !setted1 && !setted2 && !setted4) {
 			fire.move(-15, 0);
 			window.draw(fire);
-			if (fire.getPosition().x < 10 || intersect(enemy.getPosition().x, enemy.getPosition().y, 10, fire.getPosition().x, fire.getPosition().y, 20) || obstacleLeft(fire.getPosition().x - 90, fire.getPosition().y))
+			if (fire.getPosition().x < 10 || intersect(enemy.getPosition().x, enemy.getPosition().y, 10, fire.getPosition().x, fire.getPosition().y, 20) ||
+				obstacleLeft(fire.getPosition().x - 90, fire.getPosition().y)) {
 				setted3 = 0;
+				if (intersect(enemy.getPosition().x, enemy.getPosition().y, 20, fire.getPosition().x, fire.getPosition().y, 20))
+					eHealth -= 10;
+			}
 		} else
 		if (setted4 && !setted1 && !setted2 && !setted3) {
 			fire.move(15, 0);
 			window.draw(fire);
-			if (fire.getPosition().x > window.getSize().x - 10 || intersect(enemy.getPosition().x, enemy.getPosition().y, 10, fire.getPosition().x, fire.getPosition().y, 20) || obstacleRight(fire.getPosition().x + 50, fire.getPosition().y))
+			if (fire.getPosition().x > window.getSize().x - 10 || intersect(enemy.getPosition().x, enemy.getPosition().y, 10, fire.getPosition().x, fire.getPosition().y, 20) ||
+				obstacleRight(fire.getPosition().x + 50, fire.getPosition().y)) {
 				setted4 = 0;
+				if (intersect(enemy.getPosition().x, enemy.getPosition().y, 20, fire.getPosition().x, fire.getPosition().y, 20))
+					eHealth -= 10;
+			}
 		}
-
-		window.display();
+		std::string str; 
+		int fp;
+		//window.draw(exploded);
+		//window.draw(blur);
 		//std::cout << player.getSize().x << ' ' << player.getSize().y << ' ' << std::endl << enemy.getSize().x << ' ' << enemy.getSize().y << std::endl;
 		//if (intersect(player.getPosition().x, player.getPosition().y, 70, enemy.getPosition().x, enemy.getPosition().y, 70))
 		//	std::cout << "YES" << std::endl;
 		//
-		/*
-		
+
+		//
 			time = clock.getElapsedTime();
-			std::cout << 1.0f / time.asSeconds() << std::endl;
-			clock.restart().asSeconds();
-		// 
-			*/
+			fp = 1.0f / time.asSeconds();
+			clock.restart().asSeconds(); 		// 
+			str = std::to_string(fp);
+			std::string h;
+			h = std::to_string(eHealth);
+			enemyHealth.setString(h);
+			text.setString(str);
+			details.draw(text);
+			details.draw(fps);
+			details.draw(playerName);
+			details.draw(enemyName);
+			details.draw(playerHealth);
+			details.draw(enemyHealth);
+			details.draw(heartOne);
+			details.draw(heartTwo);
+			details.display();
+			if (eHealth <= 0) {
+				Win.setPosition(300,300);
+				window.clear();
+				window.draw(map);
+				window.draw(player);
+				exploded.setPosition(enemy.getPosition().x, enemy.getPosition().y);
+				window.draw(exploded);
+				window.draw(Win);
+				window.display();
+				while (window.isOpen())
+					if (sf::Keyboard::isKeyPressed(sf::Keyboard::Key::Escape)) {
+						window.close();
+						details.close();
+					}
+
+			}
+			window.display();
+
 	}
 }
